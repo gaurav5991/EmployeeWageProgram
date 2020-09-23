c0557ad5c730dda95be1ee4112e0cd1b74e0b31f
gaurav7573 <gauravenggsharma2017@gmail.com>
2020-09-23 20:00:41 +0530

    [Gaurav]ADDED: Employee Daily Wages

--- EmployeeWageUc1.java
+++ EmployeeWageUc1.java
@@ -2,15 +2,27 @@ public class EmployeeWageUc1
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee Wage Computation Program");
-
+		System.out.println("Welcome to Employee Wage Computation program");
+	
 		int IS_FULL_TIME = 1;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		int WAGE_PER_HOUR = 20;		
 		
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		
+		int empHrs =  0;
+		
+		int empWage = 0;
+
 		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+		{
+			empHrs = 8;
+		}
 		else
-			System.out.println("Employee is Absent");
+		{
+			empHrs = 0;
+		}
+		empWage = empHrs * WAGE_PER_HOUR;
+		System.out.println("Employee Wage: " + empWage);
 	}
 }
