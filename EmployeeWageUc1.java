3f6e1acb740a77a457f10f1731d57d48e147b894
gaurav7573 <gauravenggsharma2017@gmail.com>
2020-09-23 20:07:39 +0530

    [Gaurav]ADDED: Employee Wages Using Switch

--- EmployeeWageUc1.java
+++ EmployeeWageUc1.java
@@ -1,33 +1,32 @@
 public class EmployeeWageUc1
 {
+
+	public static final int IS_PART_TIME = 1;
+	
+	public static final int IS_FULL_TIME = 2;
+
+	public static final int WAGE_PER_HOUR = 20;		
+
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation program");
-	
-		int IS_PART_TIME = 1;
 
-		int IS_FULL_TIME = 2;
-
-		
-		int WAGE_PER_HOUR = 20;		
-		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		
 		int empHrs =  0;
 		
 		int empWage = 0;
 
-		if(empCheck == IS_PART_TIME)
+		switch (empCheck)
 		{
-			empHrs = 4;
-		}
-		else if(empCheck ==  IS_FULL_TIME)
-		{
-			empHrs = 8;
-		}
-		else
-		{
-			empHrs = 0;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
 		}
 		empWage = empHrs * WAGE_PER_HOUR;
 		System.out.println("Employee Wage: " + empWage);
